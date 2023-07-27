74a1bcb2fe38f7419a761cdc9637ca8d39c57c52
Amit Maharjan <amitmaharjan2002@gmail.com>
2023-07-27 14:50:51 +0545

    Messaging section

---
+++ messages/index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+if(!isset($_SESSION["uid"]))
+{
+    header("Location: ../login/");
+    exit();
+}
+
+require_once("../db_connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="../home.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
+
+<body>
+<?php
+    require_once("../components/navbar.php");?>
+  <div class="container">
+    <?php
+    require_once("../components/sidebar.php");
+    require_once("./messages.php");
+    require_once("../components/additional_section.php");
+    ?>
+    </div>
+
+</body>
+
+</html>
