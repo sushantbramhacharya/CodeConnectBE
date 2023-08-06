167e5cf447ad66fd3cd0f9f9e7f7a6d6d28b3435
Amit Maharjan <amitmaharjan2002@gmail.com>
2023-08-06 16:12:36 +0545

    Geek validation

--- components/post_scripts/geek.php
+++ components/post_scripts/geek.php
@@ -7,9 +7,30 @@ if(!isset($_SESSION["uid"]))
 }
 require_once("../../db_connect.php");
 
+if(!user_has_geeked(1,1,$conn))
+{
+
 $query="INSERT INTO geek(geekers_uid,discussion_id) VALUES(1,1)";
 
 mysqli_query($conn,$query);
 
 echo "Geeked";
+}
+else{
+    echo "already geeked";
+}
+function user_has_geeked($geekers_uid,$discussion_id,$conn) 
+{
+    $query= "SELECT * FROM geek WHERE geekers_uid = '$geekers_uid' AND discussion_id = '$discussion_id';";
+    $result= mysqli_query($conn,$query);
+    if($result->num_rows>0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
 ?>
