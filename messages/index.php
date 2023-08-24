4e06e29c0412c057dc58e5b43a36800c6e258712
Amit Maharjan <amitmaharjan2002@gmail.com>
2023-08-24 16:12:29 +0545

    messenger

--- messages/index.php
+++ messages/index.php
@@ -7,9 +7,24 @@ $sid = $_SESSION['uid'] ;
 
 require_once("../db_connect.php");
 $resp=array();
-$query="SELECT Name FROM user WHERE uid=$sid";
+$query="SELECT * FROM connections WHERE (sender_uid = $sid or reciever_uid = $sid) AND status_accepted = true";
 $result=$conn->query($query);
 $row=$result->fetch_assoc();
-$resp['name']=$row["Name"];
-echo json_encode(($resp));
+
+$sender_uid = $row["sender_uid"];
+$reciever_uid = $row["reciever_uid"];
+
+if($sender_uid === $sid)
+{
+    echo $reciever_uid;
+}
+else if($reciever_uid === $sid)
+{
+    echo $sender_uid;
+}
+
+$query_name = "SELECT Name FROM user WHERE uid = $reciever_uid OR uid = $sender_uid";
+$result=$conn->query($query_name);
+$row=$result->fetch_assoc();
+echo $result;
 ?>
